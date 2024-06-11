33085c06ac691b33a8168a8c0de62cf46eec6ee2
vritter-zhaw <145434416+vritter-zhaw@users.noreply.github.com>
2024-06-11 14:00:15 +0200

    deleted sample application

--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
-NeoPixelMatrix.initializeMatrix(DigitalPin.P0, 255)
 basic.forever(function () {
-    for (let Index = 0; Index <= 7; Index++) {
-        for (let Index2 = 0; Index2 <= 7; Index2++) {
-            NeoPixelMatrix.clear()
-            NeoPixelMatrix.setPixel(Index, Index2, 0xff0080)
-            basic.pause(100)
-        }
-    }
+	
 })
